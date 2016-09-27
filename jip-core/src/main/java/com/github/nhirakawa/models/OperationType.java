3c39f7f936524f5169803c1851849c8b5839dee5
Nick Hirakawa <nickhirakawa@gmail.com>
2016-09-27 15:37:43 -0400

    Add OperationType enum

---
+++ jip-core/src/main/java/com/github/nhirakawa/models/OperationType.java
@@ -0,0 +1,17 @@
+package com.github.nhirakawa.models;
+
+public enum OperationType {
+  CLEAR,
+  RETURN,
+  JUMP,
+  CALL,
+  SKIP,
+  ADD,
+  SET,
+  SUBTRACT,
+  SHIFT,
+  DRAW,
+  AWAIT,
+  STORE,
+  FILL
+}
