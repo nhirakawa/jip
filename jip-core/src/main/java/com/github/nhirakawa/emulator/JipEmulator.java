3f7af62f5445dc215ca501f4287b54e765edf27f
Nick Hirakawa <nickhirakawa@gmail.com>
2016-09-28 00:57:00 -0400

    Throw exception if opcode cannot be executed

--- jip-core/src/main/java/com/github/nhirakawa/emulator/JipEmulator.java
+++ jip-core/src/main/java/com/github/nhirakawa/emulator/JipEmulator.java
@@ -52,4 +52,37 @@ public class JipEmulator {
     return OpCode.of(op1 << 8 | op2);
   }
 
+  private void executeOpCode(OpCode opcode) {
+    switch (opcode) {
+      default:
+        throw new UnsupportedOperationException(opcode);
+    }
+  }
+
+  private static int getX(int op) {
+    return op & 0x0F00 >> 8;
+  }
+
+  private static int getY(int op) {
+    return op & 0x00F0 >> 4;
+  }
+
+  private static int getN(int op, int places) {
+    if (places < 1 || places > 3) {
+      throw new IllegalArgumentException(String.format("cannot get %d places", places));
+    }
+    int mask = 0xF;
+    while (places > 1) {
+      mask = (mask << 4) & 0xF;
+    }
+    return op & mask;
+  }
+
+  public static class UnsupportedOperationException extends RuntimeException {
+
+    public UnsupportedOperationException(OpCode opCode) {
+      super(String.format("%s is not supported yet", opCode));
+    }
+  }
+
 }
