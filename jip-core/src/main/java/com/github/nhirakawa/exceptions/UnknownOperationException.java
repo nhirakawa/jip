903b58b3c4ad6930694df2ff68bd5454dc638178
Nick Hirakawa <nickhirakawa@gmail.com>
2016-09-28 00:17:59 -0400

    Make message more readable

--- jip-core/src/main/java/com/github/nhirakawa/exceptions/UnknownOperationException.java
+++ jip-core/src/main/java/com/github/nhirakawa/exceptions/UnknownOperationException.java
@@ -3,6 +3,11 @@ package com.github.nhirakawa.exceptions;
 public class UnknownOperationException extends RuntimeException {
 
   public UnknownOperationException(int code) {
-    super(String.format("%d is not a known opcode", code));
+    super(String.format("0x%s is not a known opcode", hexString(code)));
   }
+
+  private static String hexString(int i) {
+    return String.format("%04x", i).toUpperCase();
+  }
+
 }
