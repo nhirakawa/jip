a18fa7deb07559f42847165f6d74c82cb93e06ec
Nick Hirakawa <nickhirakawa@gmail.com>
2016-09-27 23:50:36 -0400

    Fetch opcode from memory and print

--- jip-core/src/main/java/com/github/nhirakawa/emulator/JipEmulator.java
+++ jip-core/src/main/java/com/github/nhirakawa/emulator/JipEmulator.java
@@ -21,15 +21,34 @@ public class JipEmulator {
   @Inject
   public JipEmulator(MemoryManagementUnit memoryManagementUnit) {
     this.memoryManagementUnit = memoryManagementUnit;
+    this.indexRegister = 0;
+    this.programCounter = 0;
+    this.stackPointer = 0;
   }
 
   public void loadRom(File file) throws IOException {
     byte[] rom = IOUtils.toByteArray(file.toURI());
+    loadRom(rom);
+  }
+
+  public void loadRom(byte[] rom) {
     int[] translatedRom = new int[rom.length];
     for (int i = 0; i < rom.length; i++) {
       translatedRom[i] = (rom[i] & 0xFF);
     }
     memoryManagementUnit.writeMemory(ROM_OFFSET, translatedRom);
+  }
+
+  public void step() {
+    int opcode = fetchOpCode();
+    programCounter += 2;
+    System.out.println(String.format("0x%s", Integer.toHexString(opcode)));
+  }
+
+  private int fetchOpCode() {
+    int op1 = memoryManagementUnit.readMemory(ROM_OFFSET + programCounter);
+    int op2 = memoryManagementUnit.readMemory(ROM_OFFSET + programCounter + 1);
+    return op1 << 8 | op2;
   }
 
 }
