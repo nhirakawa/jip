1ff9268651048da4ba56f04eec8e998c1fe23a2b
Nick Hirakawa <nickhirakawa@gmail.com>
2016-09-30 21:33:51 -0400

    Draw triangle

--- jip-ui/src/main/java/com/github/nhirakawa/jip/JipApplication.java
+++ jip-ui/src/main/java/com/github/nhirakawa/jip/JipApplication.java
@@ -27,14 +27,45 @@ import static org.lwjgl.glfw.GLFW.glfwWindowHint;
 import static org.lwjgl.glfw.GLFW.glfwWindowShouldClose;
 import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
 import static org.lwjgl.opengl.GL11.GL_DEPTH_BUFFER_BIT;
+import static org.lwjgl.opengl.GL11.GL_FLOAT;
+import static org.lwjgl.opengl.GL11.GL_PROJECTION;
+import static org.lwjgl.opengl.GL11.GL_QUADS;
+import static org.lwjgl.opengl.GL11.GL_TRIANGLES;
+import static org.lwjgl.opengl.GL11.GL_UNSIGNED_INT;
+import static org.lwjgl.opengl.GL11.GL_VERTEX_ARRAY;
+import static org.lwjgl.opengl.GL11.glBegin;
 import static org.lwjgl.opengl.GL11.glClear;
 import static org.lwjgl.opengl.GL11.glClearColor;
+import static org.lwjgl.opengl.GL11.glColor3f;
+import static org.lwjgl.opengl.GL11.glDrawElements;
+import static org.lwjgl.opengl.GL11.glEnableClientState;
+import static org.lwjgl.opengl.GL11.glEnd;
+import static org.lwjgl.opengl.GL11.glFlush;
+import static org.lwjgl.opengl.GL11.glLoadIdentity;
+import static org.lwjgl.opengl.GL11.glMatrixMode;
+import static org.lwjgl.opengl.GL11.glOrtho;
+import static org.lwjgl.opengl.GL11.glVertex3f;
+import static org.lwjgl.opengl.GL11.glVertexPointer;
+import static org.lwjgl.opengl.GL11.glViewport;
+import static org.lwjgl.opengl.GL15.GL_ARRAY_BUFFER;
+import static org.lwjgl.opengl.GL15.GL_ELEMENT_ARRAY_BUFFER;
+import static org.lwjgl.opengl.GL15.GL_STATIC_DRAW;
+import static org.lwjgl.opengl.GL15.glBindBuffer;
+import static org.lwjgl.opengl.GL15.glBufferData;
+import static org.lwjgl.opengl.GL15.glGenBuffers;
 
+import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
+
+import org.lwjgl.BufferUtils;
 import org.lwjgl.glfw.GLFWErrorCallback;
 import org.lwjgl.glfw.GLFWVidMode;
 import org.lwjgl.opengl.GL;
 
 public class JipApplication {
+
+  private static final int WIDTH = 640;
+  private static final int HEIGHT = 320;
 
   private long window;
 
@@ -67,9 +98,6 @@ public class JipApplication {
     glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
     glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
 
-    int WIDTH = 300;
-    int HEIGHT = 300;
-
     window = glfwCreateWindow(WIDTH, HEIGHT, "Hello World!", 0, 0);
     if (window == 0) {
       throw new RuntimeException("Failed to create the GLFW window");
@@ -95,14 +123,44 @@ public class JipApplication {
 
   private void loop() {
     GL.createCapabilities();
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 
-    glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
+    int vbo = glGenBuffers();
+    int ibo = glGenBuffers();
+    float[] vertices = { -0.5f, -0.5f, 0.5f, -0.5f, 0.5f, 0.5f };
+    int[] indices = { 0, 1, 2 };
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, (FloatBuffer) BufferUtils.createFloatBuffer(vertices.length).put(vertices).flip(), GL_STATIC_DRAW);
+    glEnableClientState(GL_VERTEX_ARRAY);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, (IntBuffer) BufferUtils.createIntBuffer(indices.length).put(indices).flip(), GL_STATIC_DRAW);
+    glVertexPointer(2, GL_FLOAT, 0, 0L);
 
     while (!glfwWindowShouldClose(window)) {
       glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+      glViewport(0, 0, WIDTH, HEIGHT);
+      glMatrixMode(GL_PROJECTION);
+      float aspect = (float) WIDTH / HEIGHT;
+      glLoadIdentity();
+      glOrtho(-aspect, aspect, -1, 1, -1, 1);
+      glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0L);
+
       glfwSwapBuffers(window);
       glfwPollEvents();
     }
+  }
+
+  private void drawRectangle() {
+    glColor3f(200, 200, 200);
+    glBegin(GL_QUADS);
+    glVertex3f(100, 100, 0);
+    glVertex3f(100, 100, 0);
+    glVertex3f(200, 100, 0);
+    glVertex3f(200, 200, 0);
+    glVertex3f(100, 200, 0);
+    glEnd();
+    glFlush();
   }
 
   public static void main(String... args) {
