6d0365abd0cb1eb2a4336d373c015079acd911fe
Nick Hirakawa <nickhirakawa@gmail.com>
2016-10-02 00:48:08 -0400

    Almost done with debugger UI

--- jip-core/src/main/java/com/github/nhirakawa/emulator/JipDebugger.java
+++ jip-core/src/main/java/com/github/nhirakawa/emulator/JipDebugger.java
@@ -1,76 +1,67 @@
 package com.github.nhirakawa.emulator;
 
 import java.io.IOException;
-import java.net.URISyntaxException;
+import java.net.URI;
 
 import javax.swing.*;
 
 import com.github.nhirakawa.JipUtils;
-import com.github.nhirakawa.config.JipCoreModule;
-import com.google.common.io.Resources;
-import com.google.inject.Guice;
+import com.github.nhirakawa.models.OpCode;
 import com.google.inject.Inject;
-import com.google.inject.Injector;
 
-public class JipDebugger extends JFrame implements Runnable {
+public class JipDebugger extends JFrame {
 
   private final JipEmulator emulator;
-  private final JPanel panel;
-  private final JPanel textPanel;
+
   private final JTextArea memory;
   private final JTextArea registers;
-  private final JPanel currentInstructionPanel;
+  private final JTextArea stack;
+
+  private final JTextField opCode;
   private final JTextField programCounter;
-  private final JTextField instructionRegister;
-  private final JPanel controlPanel;
+  private final JTextField indexRegister;
+  private final JTextField stackPointer;
 
   @Inject
   public JipDebugger(JipEmulator emulator) {
     super("JipDebugger");
+
     this.emulator = emulator;
-    panel = new JPanel(true);
+    JPanel panel = new JPanel(true);
     panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
 
     memory = new JTextArea(16, 10);
     registers = new JTextArea(16, 10);
-    textPanel = getMemoryPanel();
+    stack = new JTextArea(16, 10);
+    JPanel textPanel = getMemoryPanel();
 
-    controlPanel = getControlPanel();
-
-    currentInstructionPanel = new JPanel();
-    currentInstructionPanel.setBorder(BorderFactory.createTitledBorder("memory"));
+    opCode = new JTextField();
     programCounter = new JTextField();
-    programCounter.setEditable(false);
-    programCounter.setText("pc");
-    instructionRegister = new JTextField();
-    instructionRegister.setEditable(false);
-    instructionRegister.setText("isr");
-    currentInstructionPanel.add(programCounter);
-    currentInstructionPanel.add(instructionRegister);
+    indexRegister = new JTextField();
+    stackPointer = new JTextField();
+    JPanel currentInstructionPanel = getCurrentInstructionPanel();
 
     panel.add(textPanel);
     panel.add(currentInstructionPanel);
-    panel.add(controlPanel);
 
     setContentPane(panel);
+    setResizable(false);
+    setLocationRelativeTo(null);
     pack();
     setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     setVisible(true);
   }
 
-  @Override
-  public void run() {
-    try {
-      emulator.loadRom(Resources.getResource("MAZE.rom").toURI());
-      loadMemory();
-    } catch (IOException | URISyntaxException e) {
-      e.printStackTrace();
-      System.exit(1);
-    }
+  public void debug(URI uri) throws IOException {
+    emulator.loadRom(uri);
+    loadMemory();
+    loadState();
   }
 
   private void emulateCycle() {
-
+    emulator.step();
+    loadMemory();
+    loadState();
   }
 
   private void loadMemory() {
@@ -85,6 +76,22 @@ public class JipDebugger extends JFrame implements Runnable {
     for (int i = 0; i < registers.length; i++) {
       this.registers.append(String.format("V%X - %s%n", i, JipUtils.toHexString(registers[i])));
     }
+
+    stack.setText("");
+    int[] stack = emulator.getStack();
+    for (int i = 0; i < stack.length; i++) {
+      this.stack.append(String.format("0x%X - %s%n", i, JipUtils.toHexString(stack[i])));
+    }
+  }
+
+  private void loadState() {
+    OpCode emulatorOpCode = emulator.getOpCode();
+    if (emulatorOpCode != null) {
+      opCode.setText(String.format("0x%04X", emulatorOpCode.getOpCode()));
+    }
+    programCounter.setText(String.format("0x%X", emulator.getProgramCounter()));
+    indexRegister.setText(String.format("0x%X", emulator.getIndexRegister()));
+    stackPointer.setText(String.format("0x%X", emulator.getStackPointer()));
   }
 
   private JPanel getMemoryPanel() {
@@ -101,35 +108,68 @@ public class JipDebugger extends JFrame implements Runnable {
     JPanel registerTextPanel = new JPanel();
     registerTextPanel.setBorder(BorderFactory.createTitledBorder("registers"));
     JScrollPane registersScrollPane = new JScrollPane(registers);
-    registersScrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
+    registersScrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER);
     registersScrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
     registerTextPanel.add(registersScrollPane);
 
+    JPanel stackTextPanel = new JPanel();
+    stackTextPanel.setBorder(BorderFactory.createTitledBorder("stack"));
+    JScrollPane stackScrollPane = new JScrollPane(stack);
+    stackScrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER);
+    stackScrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+    stackTextPanel.add(stackScrollPane);
+
     memoryPanel.add(memoryTextPanel);
     memoryPanel.add(registerTextPanel);
+    memoryPanel.add(stackTextPanel);
     return memoryPanel;
   }
 
   private JPanel getCurrentInstructionPanel() {
     JPanel controlPanel = new JPanel();
-    return controlPanel;
-  }
+    controlPanel.setBorder(BorderFactory.createTitledBorder("current state"));
 
-  private JPanel getControlPanel() {
-    JPanel buttonPanel = new JPanel();
-    buttonPanel.setBorder(BorderFactory.createTitledBorder("controls"));
+    JLabel opCodeLabel = new JLabel("op");
+    opCodeLabel.setLabelFor(opCode);
+    opCode.setEditable(false);
+    opCode.setColumns(6);
+
+    JLabel programCounterLabel = new JLabel("pc");
+    programCounterLabel.setLabelFor(programCounter);
+    programCounter.setEditable(false);
+    programCounter.setColumns(4);
+
+    JLabel indexRegisterLabel = new JLabel("index");
+    indexRegisterLabel.setLabelFor(indexRegister);
+    indexRegister.setEditable(false);
+    indexRegister.setColumns(4);
+
+    JLabel stackPointerLabel = new JLabel("sp");
+    stackPointerLabel.setLabelFor(stackPointer);
+    stackPointer.setEditable(false);
+    stackPointer.setColumns(4);
+
     JButton next = new JButton("next");
     next.addActionListener(e -> {
       emulator.step();
       loadMemory();
     });
-    buttonPanel.add(next);
-    return buttonPanel;
+
+    controlPanel.add(opCodeLabel);
+    controlPanel.add(opCode);
+
+    controlPanel.add(programCounterLabel);
+    controlPanel.add(programCounter);
+
+    controlPanel.add(indexRegisterLabel);
+    controlPanel.add(indexRegister);
+
+    controlPanel.add(stackPointerLabel);
+    controlPanel.add(stackPointer);
+
+    controlPanel.add(next);
+
+    return controlPanel;
   }
 
-  public static void main(String... args) {
-    Injector injector = Guice.createInjector(new JipCoreModule());
-    JipDebugger debugger = injector.getInstance(JipDebugger.class);
-    debugger.run();
-  }
 }
